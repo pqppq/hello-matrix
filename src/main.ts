229219537f6b7d0b510759aadb33394c89fafc67
pqppq <yutatsu0119@gmail.com>
2022-05-20 22:49:09 +0900

    define function: handleCommand

--- src/main.ts
+++ src/main.ts
@@ -18,15 +18,14 @@ AutojoinRoomsMixin.setupOnClient(client);
 // start client
 client.start().then(() => console.log("Matrix client started!"));
 
-client.on("room.message", (roomId, event) => {
+client.on("room.message", handleCommand);
+
+async function handleCommand(roomId: string, event: any): Promise<void> {
   if (!event.content) return;
   const sender: string = event.sender;
   const body: string = event.content.body;
   console.log(`${roomId}: ${sender} => ${body}`);
 
   if (body.startsWith("!bot"))
-    client.sendMessage(roomId, {
-      msgtype: "m.text",
-      body: "This is response from bot.",
-    });
-});
+    await client.replyNotice(roomId, event, "This is the reply from bot.");
+}
